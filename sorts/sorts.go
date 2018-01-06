92ddc691dc1432394e74f2b1eb9d30b0a290a676
tendingnut <tendingnut20@hotmail.co.uk>
2018-01-06 19:16:57 +0000

    NewList constructor added to create random slice, and slice types changed to int from float64

--- sorts/sorts.go
+++ sorts/sorts.go
@@ -4,28 +4,25 @@ package sorts
 
 import (
 	"math"
+	"math/rand"
+	"time"
 )
 
-type list interface {
-	Merge()
-	Insertion()
-}
+type ranList []int
 
-type randomList struct {
-	n int
-}
-
-//func NewList() *list {
-//	return &list{5}
-//}
-
-func NewListRand() {
-
+//Newlist returns a list of n random ints in the range 0 to lim.
+func NewList(n int, lim int) ranList {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	list := make(ranList, n)
+	for i := 0; i < n; i++ {
+		list[i] = r.Intn(lim)
+	}
+	return list
 }
 
 //Reverse the ordering of elements.
-func Reverse(slice []float64) []float64 {
-	list := make([]float64, len(slice))
+func Reverse(slice []int) []int {
+	list := make([]int, len(slice))
 	copy(list, slice)
 	for i := 0; i < len(slice); i++ {
 		list[i] = slice[len(slice)-i-1]
@@ -34,8 +31,8 @@ func Reverse(slice []float64) []float64 {
 }
 
 //Insertion sort has n^2 complexity.
-func Insertion(slice []float64) []float64 {
-	list := make([]float64, len(slice))
+func Insertion(slice []int) []int {
+	list := make([]int, len(slice))
 	copy(list, slice)
 	for j := 1; j < len(list); j++ {
 		key := list[j]
@@ -50,8 +47,8 @@ func Insertion(slice []float64) []float64 {
 }
 
 //Merge sort has nlg(n) complexity.
-func Merge(slice []float64) []float64 {
-	list := make([]float64, len(slice))
+func Merge(slice []int) []int {
+	list := make([]int, len(slice))
 	copy(list, slice)
 	mergeSort(list, 0, len(list)-1)
 	return list
@@ -59,7 +56,7 @@ func Merge(slice []float64) []float64 {
 
 //mergeSort divides the list recursively into sub-sections by calling mergeSort(),
 //and calls 'merge()' on each sub-section (p..r) to sort.
-func mergeSort(list []float64, p, r int) { //p is first element, r is last element
+func mergeSort(list []int, p, r int) { //p is first element, r is last element
 	if p < r { // if more than 1 element in list, otherwise already sorted
 		q := int(math.Floor(float64((p + r) / 2))) // middle element used to split list
 		mergeSort(list, p, q)
@@ -70,8 +67,8 @@ func mergeSort(list []float64, p, r int) { //p is first element, r is last eleme
 
 //called on a sub-section of list, where each half is sorted (p..q) and (q+1..r),
 //and put into separate slices, which are used to compare and sort entire sub-section (p..r)
-func merge(list []float64, p, q, r int) {
-	var A, B []float64
+func merge(list []int, p, q, r int) {
+	var A, B []int
 	//loops fill up slices with appropriate values from list
 	for i := p; i <= q; i++ {
 		A = append(A, list[i])
